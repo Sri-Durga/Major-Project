d5a4ae657eacba1b6334b02be556b34ea1ee78a7
Sri <57977206+Sri-Durga@users.noreply.github.com>
2021-06-10 13:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import streamlit as st
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-vs = SentimentIntensityAnalyzer()
+import joblib
+model = joblib.load('sentiment')
 st.title('Sentiment Analysis')
 ip = st.text_input("Enter the review")
-pred = vs.polarity_scores(ip)['compound']
-if pred >= 0.4:
-  op = "Positive review"
-elif pred <= -0.05:
-  op = "Negative review"
-else:
-  op = "Neutral review"
-
+op = model.predict([ip])
 if st.button('Predict'):
-  st.title(op)
-
-  
-  
+  st.title(op[0])
